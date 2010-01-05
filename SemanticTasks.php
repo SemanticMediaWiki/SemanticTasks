dd880883d3fe0efab0dd9d8c9a4846b91c62f5d7
Yaron Koren <yaron@users.mediawiki.org>
2010-01-05 17:16:24 +0000

    Version 1.2: functions moved from ST_Notify_Assignment.php to SemanticTasks.classes.php

--- SemanticTasks.php
+++ SemanticTasks.php
@@ -18,19 +18,24 @@ $wgExtensionCredits['parserhook'][] = array(
 	'path' => __FILE__,
 	'name' => 'SemanticTasks',
 	'author' => 'Steren Giannini',
-	'version' => '1.1.1',
-	'url' => 'http://www.creativecommons.org', // FIXME: URL should point to a page about this extension
+	'version' => '1.2',
+	'url' => 'http://www.mediawiki.org/wiki/Extension:Semantic_Tasks',
 	'description' => 'E-mail notifications for assigned or updated tasks',
 	'descriptionmsg' => 'semantictasks-desc',
 );
 
 // Do st_SetupExtension after the mediawiki setup, AND after SemanticMediaWiki setup
 // FIXME: only hook added is ArticleSaveComplete. There appears to be no need for this.
-$wgExtensionFunctions[] = 'st_SetupExtension';
+$wgExtensionFunctions[] = 'SemanticTasksSetupExtension';
 
 // i18n
 $wgExtensionMessagesFiles['SemanticTasks'] = dirname( __FILE__ ) . '/SemanticTasks.i18n.php';
 
-// FIXME: Use $wgAutoloadClasses
-// ST_Notify_Assignment.php
-require_once( dirname( __FILE__ ) . "/ST_Notify_Assignment.php" );
+$dir = dirname( __FILE__ ) . '/';
+$wgAutoloadClasses['SemanticTasksMailer'] = $dir . 'SemanticTasks.classes.php';
+
+function SemanticTasksSetupExtension() {
+        global $wgHooks;
+        $wgHooks['ArticleSaveComplete'][] = 'SemanticTasksMailer::mailAssigneesUpdatedTask';
+        return true;
+}
