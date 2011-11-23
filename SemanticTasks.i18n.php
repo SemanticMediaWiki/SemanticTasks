4752b27b4445a66686bf731ee84cde5801bdfa8b
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-11-23 18:43:52 +0000

    Localisation updates from http://translatewiki.net.

--- SemanticTasks.i18n.php
+++ SemanticTasks.i18n.php
@@ -778,7 +778,7 @@ $3',
 	'semantictasks-diff-message' => 'Toto sú rozdiely:',
 );
 
-/** Serbian Cyrillic ekavian (‪Српски (ћирилица)‬)
+/** Serbian (Cyrillic script) (‪Српски (ћирилица)‬)
  * @author Михајло Анђелковић
  */
 $messages['sr-ec'] = array(
@@ -790,7 +790,7 @@ $messages['sr-ec'] = array(
 	'semantictasks-diff-message' => 'Овде су разлике:',
 );
 
-/** Serbian Latin ekavian (‪Srpski (latinica)‬)
+/** Serbian (Latin script) (‪Srpski (latinica)‬)
  * @author Michaello
  */
 $messages['sr-el'] = array(
