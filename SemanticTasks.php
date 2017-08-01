8627cb4d00463ea7f8d81685ae788d66169bf675
Karsten Hoffmeyer <karsten@hoffmeyer.info>
2017-08-01 23:21:13 +0200

    Do some maintenance
    * Remove I18n php shim
    * Update extension's group
    * Extend author's array
    * Update RELEASE-NOTES
    * Update INSTALL
    * Improve code documentation
    * Bump version
    
    Note: This is breaking for MW 1.22.x and lower.
    
    Bug:T168353
    Change-Id: Idfb9e11d91bcc26c5e7f832e25b8dc33828945b0

--- SemanticTasks.php
+++ SemanticTasks.php
@@ -1,46 +1,45 @@
 <?php
 
+// Ensure that the script cannot be executed outside of MediaWiki.
 if ( !defined( 'MEDIAWIKI' ) ) {
-	echo 'Not a valid entry point';
-	exit( 1 );
+	die( 'This is an extension to MediaWiki and cannot be run standalone.' );
 }
 
+// Ensure that Semantic MediaWiki is installed.
 if ( !defined( 'SMW_VERSION' ) ) {
-	echo 'This extension requires Semantic MediaWiki to be installed.';
-	exit( 1 );
+	die( 'This extension requires Semantic MediaWiki to be installed.' );
 }
 
-# This is the path to your installation of SemanticTasks as
-# seen from the web. Change it if required ($wgScriptPath is the
-# path to the base directory of your wiki). No final slash. It appears to be unused.
+// This is the path to your installation of SemanticTasks as seen from the web.
+// Change it if required ($wgScriptPath is the path to the base directory of
+// your wiki). No final slash. It appears to be unused.
 $stScriptPath = $wgScriptPath . '/extensions/SemanticTasks';
 
-# Set to true to notify users when they are unassigned from a task
+// Set to true to notify users when they are unassigned from a task
 $wgSemanticTasksNotifyIfUnassigned = false;
 
-
-# Extension credits
-$wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'other'][] = array(
+// Display extension properties on MediaWiki.
+$wgExtensionCredits['semantic'][] = array(
 	'path' => __FILE__,
 	'name' => 'SemanticTasks',
 	'author' => array(
 		'Steren Giannini',
 		'Ryan Lane',
-		'Ike Hecht'
+		'Ike Hecht',
+		'...'
 	),
-	'version' => '1.6.0',
+	'version' => '1.7.0',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Semantic_Tasks',
 	'descriptionmsg' => 'semantictasks-desc',
 	'license-name' => 'GPL-2.0+'
 );
 
-// i18n
+// Register extension messages and other localisation.
 $wgMessagesDirs['SemanticTasks'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['SemanticTasks'] = __DIR__ . '/SemanticTasks.i18n.php';
 
-// Autoloading
+// Load extension's classes.
 $wgAutoloadClasses['SemanticTasksMailer'] = __DIR__ . '/SemanticTasks.classes.php';
 
-// Hooks
+// Register extension hooks.
 $wgHooks['PageContentSaveComplete'][] = 'SemanticTasksMailer::mailAssigneesUpdatedTask';
 $wgHooks['PageContentSave'][] = 'SemanticTasksMailer::findOldValues';
