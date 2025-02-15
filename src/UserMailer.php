4f3fafe169d343e5f314b9f99985202cf604f4e2
thomas-topway-it <topway.it@mail.com>
2025-02-15 13:23:15 +0400

    fix unauthorized from

--- src/UserMailer.php
+++ src/UserMailer.php
@@ -2,6 +2,10 @@
 
 namespace ST;
 
+use MailAddress;
+use MediaWiki\MainConfigNames;
+use MediaWiki\MediaWikiServices;
+
 class UserMailer {
 
 	private $userMailer;
@@ -19,6 +23,19 @@ class UserMailer {
 	 * @throws \MWException
 	 */
 	function send( $to, $from, $subject, $body, $options = [] ) {
-		$this->userMailer->send( $to, $from, $subject, $body, $options );
+		// @see User -> sendMail
+		$passwordSender = MediaWikiServices::getInstance()->getMainConfig()
+			->get( MainConfigNames::PasswordSender );
+
+		$sender = new MailAddress( $passwordSender,
+			wfMessage( 'emailsender' )->inContentLanguage()->text() );
+
+		$this->userMailer->send( $to, $sender, $subject, $body, [
+			'replyTo' => $from,
+		] );
+
+		// *** the following may fail since $from is not the real
+		// sender
+		// $this->userMailer->send( $to, $from, $subject, $body, $options );
 	}
 }
